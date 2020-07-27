f590f9529fe8ca3806796d7f11cdf7b6c3853d2e
Akankshaswami215 <akankshaswami1999@gmail.com>
2020-07-27 12:14:27 +0530

    File added

---
+++ New.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akanksha;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author AkankshaSwami
+ */
+public class New {
+    public static void main(String args[]){
+        /*String arr="hello i am akaksha";
+        String [] splitStr=arr.split("");
+        int i=0;
+        while(splitStr[i]){
+            if(Arrays.asList(ArrayEX).contains(arr[i]))
+            {
+                System.out.println("Found");
+            }
+            i++;*/
+        int a=0,b=10;
+        a=-b;
+        System.out.println(a+","+b);
+        
+        }
+    }
+
